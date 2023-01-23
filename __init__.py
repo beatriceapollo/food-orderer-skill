21471a8207d740e5361abb4d97d076b8dfd85269
Beatrice Apollo <beatrice.apollo.00@gmail.com>
2023-01-23 12:07:08 +0100

    test 1

--- __init__.py
+++ __init__.py
@@ -1,13 +1,20 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler, AdaptIntent
+from mycroft.skills.context import adds_context, removes_context
 
 
 class FoodOrderer(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('orderer.food.intent')
-    def handle_orderer_food(self, message):
+    @intent_handler('orderer.food.intent')
+    def handle_food(self, message):
         self.speak_dialog('orderer.food')
+
+        @intent_handler(AdaptIntent("").require('type'))
+        def type(self, message):
+            food_type = message.data.get('type')
+            self.speak('okay I will order a {}'.format(food_type))
+
 
 
 def create_skill():
